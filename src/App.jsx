b67e71de9d1dc31272832be9511d90075c2da5b0
iamtasnim <261-15-338@diu.edu.bd>
2026-06-11 17:18:23 +0600

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import './App.css'
+import { FaRegDotCircle } from "react-icons/fa";
 
 function App() {
   
 
   return (
     <>
-      <div className="bg-b"></div>
+      <div className="bg-gray-500 h-screen w-screen flex justify-center items-center ">
+<div className=" bg-white h-[65vh] w-[50vw] rounded-2xl">
+ <div className=" flex justify-between p-6 bg-black rounded-2xl">
+   <p className='text-left font-bold text-[18px] text-white'>Todo List</p>
+  <button className='bg-white text-black font-bold px-4 py-1 rounded-xl'>New +</button>
+ </div>
+<div className="flex justify-center py-12 px-[70px]">
+  <div className="flex flex-wrap w-full gap-4">
+     
+        <p className='bg-gray-200  w-full p-4 rounded-xl'>Quiz-01</p>
+        
+        <p className='bg-gray-200  w-full p-4 rounded-xl'>Quiz-02</p>
+
+    <p className='bg-gray-200  w-full p-4 rounded-xl'>Quiz-03</p>
+      <p className='bg-gray-200  w-full p-4 rounded-xl'>Quiz-04</p>
+        <p className='bg-gray-200  w-full p-4 rounded-xl'>Quiz-05</p>
+<FaRegDotCircle />
+  
+  </div>
+
+</div>
+</div>
+
+
+
+
+      </div>
     </>
   )
 }
